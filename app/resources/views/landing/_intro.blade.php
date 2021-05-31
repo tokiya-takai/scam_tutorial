2d6b4cf19a732de253ce05d409e107904531e0e7
tokiya-takai <t.orca.dolphin@gmail.com>
2021-05-31 15:38:12 +0900

    Intro info

--- app/resources/views/landing/_intro.blade.php
+++ app/resources/views/landing/_intro.blade.php
@@ -1,10 +1,16 @@
 <div id="landing-intro">
-  <div class="landing-intro">
-    <div class="landing-intro-info">
-      <img src="/images/intro.png" alt="intro">
-      <h3><span>まずはここから<br>始めてみましょう。</span></h3>
-      <span>会員登録は不要です。</span>
+  <div class="landing-intro-plan">
+    <div class="landing-intro-inner">
+      <div class="landing-intro-info">
+        <div class="landing-intro-info-image">
+          <img src="/images/intro.png" alt="intro">
+        </div>
+        <div class="landing-intro-info-content">
+          <h3>まずはここから<br>始めてみましょう</h3>
+          <span>会員登録は不要です。</span>
+        </div>
+      </div>
+      <div class="landing-intro-inisial"></div>
     </div>
-    <div class="landing-intro-inisial"></div>
   </div>
 </div>
