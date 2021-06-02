19abb75821b8cfc29e30ed63e762a86b686fd83b
tokiya-takai <t.orca.dolphin@gmail.com>
2021-06-02 21:35:03 +0900

    Congratulations fix

--- app/resources/js/components/CongratulationsComponent.vue
+++ app/resources/js/components/CongratulationsComponent.vue
@@ -4,7 +4,13 @@
       <h1>Congratulations!</h1>
       <div class="congratulations-lesson1-body">
         <p>お疲れ様でした！Lesson1をクリアしました！</p>
-        <button class="congratulations-lesson1-btn">トップページに戻る</button>
+        <a href="/"><button class="congratulations-lesson1-btn">トップページに戻る</button></a>
+      </div>
+      <div class="congratulations-lesson1-indicator-container">
+        <div class="congratulations-lesson1-indicator-background">
+          <div class="congratulations-lesson1-indicator" :style="'width:' + width + '%;'"></div>
+        </div>
+        <span class="congratulations-lesson1-all-lessons">1/1</span>
       </div>
     </div>
     <div v-else>
@@ -15,8 +21,27 @@
 
 <script>
 export default {
+  data() {
+    return {
+      width: 0,
+    }
+  },
+  mounted() {
+    setTimeout(this.updateIndicator, 300);
+  },
   props: {
     lessonStatus: Boolean,
   },
+  methods: {
+    updateIndicator() {
+      this.width = 100;
+    },
+  },
 }
 </script>
+
+<style>
+.congratulations-lesson1-indicator {
+  transition: width 1.5s;
+}
+</style>
