23f8d1bda6f2d46aab3fa09a1c18c204fb56141b
tokiya-takai <t.orca.dolphin@gmail.com>
2021-06-03 21:16:39 +0900

    LP footer for pc

--- app/resources/views/layouts/_footer.blade.php
+++ app/resources/views/layouts/_footer.blade.php
@@ -1,6 +1,14 @@
 <footer id="footer">
   <div class="footer-container">
-    <img src="/images/full-logo.png" alt="">
-    <span>このアプリは個人が運営しています。</span>
+    <div class="footer-content1">
+      <img class="footer-content1-logo" src="/images/full-logo.png" alt="footer-logo">
+    </div>
+    <div class="footer-content2">
+      <p class="footer-content2-title1">サポート</p>
+      <p class="footer-content2-body"><a href="#">プライバシーポリシー</a></p>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <span>&copy;2021 i-Lite</span>
   </div>
 </footer>
