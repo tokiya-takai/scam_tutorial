368f55d371ce7e48034210d33f9cd13659278408
tokiya-takai <t.orca.dolphin@gmail.com>
2021-06-03 23:46:59 +0900

    Set up a withdrawal link

--- app/resources/views/layouts/_footer.blade.php
+++ app/resources/views/layouts/_footer.blade.php
@@ -3,10 +3,14 @@
     <div class="footer-content1">
       <img class="footer-content1-logo" src="/images/full-logo.png" alt="footer-logo">
     </div>
-    <div class="footer-content2">
-      <p class="footer-content2-title1">サポート</p>
-      <p class="footer-content2-body"><a href="{{ route('privacyPolicy') }}">プライバシーポリシー</a></p>
-      <p class="footer-content2-body"><a href="{{ route('termsOfUse') }}">利用規約</a></p>
+    <div class="footer-content">
+      <p class="footer-content-title1">サービス</p>
+      <p class="footer-content-body"><a href="#">退会手続</a></p>
+    </div>
+    <div class="footer-content">
+      <p class="footer-content-title1">サポート</p>
+      <p class="footer-content-body"><a href="{{ route('privacyPolicy') }}">プライバシーポリシー</a></p>
+      <p class="footer-content-body"><a href="{{ route('termsOfUse') }}">利用規約</a></p>
     </div>
   </div>
   <div class="footer-copyright">
