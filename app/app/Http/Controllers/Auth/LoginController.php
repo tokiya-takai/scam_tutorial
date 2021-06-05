7f593e0f7a6b7efae1ff9f2de0db28ed191c9895
tokiya-takai <t.orca.dolphin@gmail.com>
2021-06-05 21:22:32 +0900

    Error Class 'Laravel\Socialite\SocialiteServiceProvider' not found fix

--- app/app/Http/Controllers/Auth/LoginController.php
+++ app/app/Http/Controllers/Auth/LoginController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use App\Models\User;
-use Laravel\Socialite\Facades;
+use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
