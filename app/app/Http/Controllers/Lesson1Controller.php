059621f6e872fe255b75e5c7cc6caa00b9b131b9
tokiya-takai <t.orca.dolphin@gmail.com>
2021-06-05 20:54:19 +0900

    Lesson browser back countermeasures

--- app/app/Http/Controllers/Lesson1Controller.php
+++ app/app/Http/Controllers/Lesson1Controller.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class Lesson1Controller extends Controller
 {
     public function index(){
+        header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0");
+        header("Pragma: no-cache");
         return view('lesson1.index');
     }
 
