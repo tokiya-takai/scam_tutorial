b36f52ee8942f651275f5cfe3aa773f427b1b7fd
tokiya-takai <t.orca.dolphin@gmail.com>
2021-06-06 23:36:03 +0900

    flash to register fix

--- app/resources/views/landing/_flash.blade.php
+++ app/resources/views/landing/_flash.blade.php
@@ -4,7 +4,7 @@
       <span><b>i-Liteをご利用頂きありがとうございます！</b><br>アプリはこれからよりアップデートされます。<br>もしよかったら会員登録をお願いします！</span>
     </div>
     <div class="landing-flash-content">
-      <div class="to-register"><a href="{{ route('login') }}"><button class="to-register-btn flash-select-btn">登録に進む</button></a></div>
+      <div class="to-register"><a href="{{ route('register') }}"><button class="to-register-btn flash-select-btn">登録に進む</button></a></div>
       <div class="no-thanks"><a href="/"><button class="no-thanks-btn flash-select-btn">いいえ、結構です</button></a></div>
     </div>
   </div>
