98ab67a739536d9f25cd3ea75c97540a70c05a1a
tokiya-takai <t.orca.dolphin@gmail.com>
2021-06-10 01:41:00 +0900

    Add description

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -7,7 +7,10 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>i-Lite</title>
+    <title>i-Lite | 触って動かしてITリテラシーを学ぶならi-Lite</title>
+    <meta name="description" content="i-Lite(アイリテ)は、オンライン演習型ITリテラシー学習アプリです。インターネットを使った、
+    サイバー攻撃と言われる身近な脅威を、「実践的に、楽しく」学べるよう、サポートします。パソコンやスマートフォンを使うのが当たり前になった時代だからこそ、
+    ちょっとした知識と心がけを身に付けて、安心・安全にITを使えるようになってもらうのが私達の願いです。">
 
     <!-- Scripts -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
