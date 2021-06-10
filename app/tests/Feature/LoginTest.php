12bf3419cb0d34306e1da6b5b7606cd9250aab3e
tokiya-takai <t.orca.dolphin@gmail.com>
2021-06-10 17:43:14 +0900

    Login test

---
+++ app/tests/Feature/LoginTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class LoginTest extends TestCase
+{
+    use DatabaseTransactions;
+    
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        // ユーザーを作成
+        $user = User::factory()->create([
+            'password'  => bcrypt('testpassword123')
+        ]);
+
+        // 認証されないことを確認
+        $this->assertFalse(Auth::check());
+
+        // ログインを実行
+        $response = $this->post('login', [
+            'email'    => $user->email,
+            'password' => 'testpassword123'
+            //先ほど設定したパスワードを入力
+        ]);
+
+        // 認証されていることを確認
+        $this->assertTrue(Auth::check());
+
+        // ログイン後にホームページにリダイレクトされるのを確認
+        $response->assertRedirect('home');
+
+    }
+}
