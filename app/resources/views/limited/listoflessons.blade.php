bcd8e70344d9fcf502c6ece21c0e7f8b8a242a7f
tokiya-takai <t.orca.dolphin@gmail.com>
2021-06-12 13:51:02 +0900

    Reflected in list of lessons

--- app/resources/views/limited/listoflessons.blade.php
+++ app/resources/views/limited/listoflessons.blade.php
@@ -2,10 +2,21 @@
 
 @section('content')
 
-    @include('limited._header')
-    
-    <list-of-lessons-component></list-of-lessons-component>
-
-    @include('layouts._footer')
+    <div id="limited-inner" class="limited-inner">
+        <div class="limited-main">
+            @include('limited._header')
+        
+            <list-of-lessons-component></list-of-lessons-component>
+        
+            @include('layouts._footer')
+        </div>
+        <div id="limited-menu" class="limited-menu">
+            <ul class="limited-menu-list">
+                <li class="limited-menu-list-user">{{ Auth::user()->name }}</li>
+                <li class="limited-menu-list-others"><a href="/dashboard">ダッシュボード</a></li>
+                <li class="limited-menu-list-others"><a href="/listoflessons">レッスン一覧</a></li>
+            </ul>
+        </div>
+    </div>
     
 @endsection
